67f70e24b88a376a174353e0fda144cd0db08640
Barath <bksderode@gmail.com>
2021-11-02 19:06:34 +0900

    update

--- Lambda/lambda_function.py
+++ Lambda/lambda_function.py
@@ -6,5 +6,5 @@ def lambda_handler(event, context):
     print(event)
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from lambda-----!')
+        'body': json.dumps('Hello from lambda function')
     }
