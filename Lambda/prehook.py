3f2e7c450e2c3f3ee53a826564a230dada671a2c
Barath <bksderode@gmail.com>
2021-11-02 18:53:05 +0900

    update

--- Lambda/prehook.py
+++ Lambda/prehook.py
@@ -6,11 +6,7 @@ def lambda_handler(event, context):
 	deploymentId = event['DeploymentID']
 	exID = event['LifecycleEventHookExecutionId']
 	client = boto3.client('codedeploy')
-	params = {
-            ,
-            lifecycleEventHookExecutionId,
-            status // status can be "Succeeded" or "Failed"
-        }
+
 	client.put_lifecycle_event_hook_execution_status(deploymentId=deploymentId,lifecycleEventHookExecutionId=exID,status='Succeeded')
     return {
         'statusCode': 200,
