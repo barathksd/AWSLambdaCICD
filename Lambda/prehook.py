48bd556cbd9e0cbe8b7a683e9d29862b14a33e65
Barath <bksderode@gmail.com>
2021-11-04 13:27:03 +0900

    update

--- Lambda/prehook.py
+++ Lambda/prehook.py
@@ -6,7 +6,10 @@ def lambda_handler(event, context):
 	deploymentId = event['DeploymentID']
 	exID = event['LifecycleEventHookExecutionId']
 	client = boto3.client('codedeploy')
-
+	
+	# Lambda function status confirmation
+	# ...
+	
 	client.put_lifecycle_event_hook_execution_status(deploymentId=deploymentId,lifecycleEventHookExecutionId=exID,status='Succeeded')
     return {
         'statusCode': 200,
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # AWSLambdaCICD
+There are 4 stages in the pipeline,
+1. Source repository 
+	- CodeCommit, GitHub, GitLab etc.
+
+2. Build - CodeBuild
+	- References buildspec.yaml
+	- Create outputTemplate.yaml using template.yml
+	- Create CloudFormation Stack
+
+3. Create or replace ChangeSet (CloudFormation)
+	- references outputTemplate.yaml
+
+4. Execute ChangeSet (CloudFormation)
+
